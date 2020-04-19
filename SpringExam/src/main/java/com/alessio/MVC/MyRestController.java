7af373df99bfda9a029e4f466f24762138c09b01
alessio <mazzttaalessio93@gmail.com>
2020-04-19 15:25:19 +0200

    added rest controller

---
+++ SpringExam/src/main/java/com/alessio/MVC/MyRestController.java
@@ -0,0 +1,14 @@
+package com.alessio.MVC;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class MyRestController {
+	@GetMapping("/")
+	public String getRest() {
+		return "test rest...";
+	}
+}
